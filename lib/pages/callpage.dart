36a83ab0207d9b9bea16c00b835607ff57684947
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-03-11 21:34:57 +0530

    ⏫ Upgraded Agora RTC Engine and Moved all the dependencies to null safer versions

--- lib/pages/callpage.dart
+++ lib/pages/callpage.dart
@@ -1,7 +1,9 @@
-import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:agora_voice_bot/utils/appID.dart';
 import 'package:agora_voice_bot/widgets/rtm.dart';
 import 'package:flutter/material.dart';
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 
 class CallPage extends StatefulWidget {
   final String userName;
@@ -15,14 +17,15 @@ class _CallPageState extends State<CallPage> {
   static final _users = <int>[];
   final _infoStrings = <String>[];
   bool muted = false;
+  RtcEngine _engine;
 
   @override
   void dispose() {
     // clear users
     _users.clear();
     // destroy sdk
-    AgoraRtcEngine.leaveChannel();
-    AgoraRtcEngine.destroy();
+    _engine.leaveChannel();
+    _engine.destroy();
     super.dispose();
   }
 
@@ -46,81 +49,66 @@ class _CallPageState extends State<CallPage> {
 
     await _initAgoraRtcEngine();
     _addAgoraEventHandlers();
-    await AgoraRtcEngine.enableWebSdkInteroperability(true);
-    await AgoraRtcEngine.setParameters(
+    await _engine.setParameters(
         '''{\"che.video.lowBitRateStreamParameter\":{\"width\":320,\"height\":180,\"frameRate\":15,\"bitRate\":140}}''');
-    await AgoraRtcEngine.joinChannel(null, widget.channelName, null, 0);
+    await _engine.joinChannel(null, widget.channelName, null, 0);
   }
 
   /// Add agora sdk instance and initialize
   Future<void> _initAgoraRtcEngine() async {
-    await AgoraRtcEngine.create(appID);
-    await AgoraRtcEngine.enableVideo();
+    _engine = await RtcEngine.create(appID);
+    await _engine.enableVideo();
   }
 
   /// agora event handlers
   void _addAgoraEventHandlers() {
-    AgoraRtcEngine.onError = (dynamic code) {
-      setState(() {
-        final info = 'onError: $code';
-        _infoStrings.add(info);
-      });
-    };
-
-    /// Use this function to obtain the uid of the person who joined the channel 
-    AgoraRtcEngine.onJoinChannelSuccess = (
-      String channel,
-      int uid,
-      int elapsed,
-    ) {
-      setState(() {
-        final info = 'onJoinChannel: $channel, uid: $uid';
-        _infoStrings.add(info);
-      });
-    };
-
-    AgoraRtcEngine.onLeaveChannel = () {
-      setState(() {
-        _infoStrings.add('onLeaveChannel');
-        _users.clear();
-      });
-    };
-
-    AgoraRtcEngine.onUserJoined = (int uid, int elapsed) {
-      setState(() {
-        final info = 'userJoined: $uid';
-        _infoStrings.add(info);
-        _users.add(uid);
-      });
-    };
-
-    AgoraRtcEngine.onUserOffline = (int uid, int reason) {
-      setState(() {
-        final info = 'userOffline: $uid';
-        _infoStrings.add(info);
-        _users.remove(uid);
-      });
-    };
-
-    AgoraRtcEngine.onFirstRemoteVideoFrame = (
-      int uid,
-      int width,
-      int height,
-      int elapsed,
-    ) {
-      setState(() {
-        final info = 'firstRemoteVideo: $uid ${width}x $height';
-        _infoStrings.add(info);
-      });
-    };
+    _engine.setEventHandler(RtcEngineEventHandler(
+      error: (code) {
+        setState(() {
+          final info = 'onError: $code';
+          _infoStrings.add(info);
+        });
+      },
+      joinChannelSuccess: (channel, uid, elapsed) {
+        setState(() {
+          final info = 'onJoinChannel: $channel, uid: $uid';
+          _infoStrings.add(info);
+        });
+      },
+      leaveChannel: (stats) {
+        setState(() {
+          _infoStrings.add('onLeaveChannel');
+          _users.clear();
+        });
+      },
+      userJoined: (uid, elapsed) {
+        setState(() {
+          final info = 'userJoined: $uid';
+          _infoStrings.add(info);
+          _users.add(uid);
+        });
+      },
+      userOffline: (uid, reason) {
+        setState(() {
+          final info = 'userOffline: $uid , reason: $reason';
+          _infoStrings.add(info);
+          _users.remove(uid);
+        });
+      },
+      firstRemoteVideoFrame: (uid, width, height, elapsed) {
+        setState(() {
+          final info = 'firstRemoteVideoFrame: $uid';
+          _infoStrings.add(info);
+        });
+      },
+    ));
   }
 
   /// Helper function to get list of native views
   List<Widget> _getRenderViews() {
-    final List<AgoraRenderWidget> list = [
-      AgoraRenderWidget(0, local: true, preview: true),
-    ];
-    _users.forEach((int uid) => list.add(AgoraRenderWidget(uid)));
+    final List<StatefulWidget> list = [];
+    list.add(RtcLocalView.SurfaceView());
+    _users.forEach((int uid) => list.add(RtcRemoteView.SurfaceView(uid: uid)));
     return list;
   }
 
@@ -187,65 +175,68 @@ class _CallPageState extends State<CallPage> {
       },
     );
   }
+
   Widget _toolbar() {
-  
-  return Container(
-    alignment: Alignment.bottomCenter,
-    padding: const EdgeInsets.symmetric(vertical: 48),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        RawMaterialButton(
-          onPressed: _onToggleMute,
-          child: Icon(
-            muted ? Icons.mic_off : Icons.mic,
-            color: muted ? Colors.white : Colors.blueAccent,
-            size: 20.0,
-          ),
-          shape: CircleBorder(),
-          elevation: 2.0,
-          fillColor: muted ? Colors.blueAccent : Colors.white,
-          padding: const EdgeInsets.all(12.0),
-        ),
-        RawMaterialButton(
-          onPressed: () => _onCallEnd(context),
-          child: Icon(
-          Icons.call_end,
-          color: Colors.white,
-          size: 35.0,
-        ),
-        shape: CircleBorder(),
-          elevation: 2.0,
-          fillColor: Colors.redAccent,
-          padding: const EdgeInsets.all(15.0),
-        ),
-        RawMaterialButton(
-          onPressed: _onSwitchCamera,
-          child: Icon(
-            Icons.switch_camera,
-            color: Colors.blueAccent,
-            size: 20.0,
-          ),
-          shape: CircleBorder(),
-          elevation: 2.0,  
-          fillColor: Colors.white,
-          padding: const EdgeInsets.all(12.0),
-        ),
-        RawMaterialButton(
-          onPressed: () => Navigator.push(context,
-            MaterialPageRoute(
-              builder: (context) => RealTimeMessaging(widget.channelName, widget.userName),
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: const EdgeInsets.symmetric(vertical: 48),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RawMaterialButton(
+            onPressed: _onToggleMute,
+            child: Icon(
+              muted ? Icons.mic_off : Icons.mic,
+              color: muted ? Colors.white : Colors.blueAccent,
+              size: 20.0,
             ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: muted ? Colors.blueAccent : Colors.white,
+            padding: const EdgeInsets.all(12.0),
           ),
-          child: Icon(Icons.chat,
-          color: Colors.blueAccent,
-          size: 20,
+          RawMaterialButton(
+            onPressed: () => _onCallEnd(context),
+            child: Icon(
+              Icons.call_end,
+              color: Colors.white,
+              size: 35.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: Colors.redAccent,
+            padding: const EdgeInsets.all(15.0),
           ),
-          shape: CircleBorder(),
-          elevation: 2.0,
-          fillColor: Colors.white,
-          padding: const EdgeInsets.all(12.0),
-        ),
+          RawMaterialButton(
+            onPressed: _onSwitchCamera,
+            child: Icon(
+              Icons.switch_camera,
+              color: Colors.blueAccent,
+              size: 20.0,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: Colors.white,
+            padding: const EdgeInsets.all(12.0),
+          ),
+          RawMaterialButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    RealTimeMessaging(widget.channelName, widget.userName),
+              ),
+            ),
+            child: Icon(
+              Icons.chat,
+              color: Colors.blueAccent,
+              size: 20,
+            ),
+            shape: CircleBorder(),
+            elevation: 2.0,
+            fillColor: Colors.white,
+            padding: const EdgeInsets.all(12.0),
+          ),
         ],
       ),
     );
@@ -254,18 +245,18 @@ class _CallPageState extends State<CallPage> {
   void _onCallEnd(BuildContext context) {
     Navigator.pop(context);
   }
+
   void _onToggleMute() {
     setState(() {
       muted = !muted;
     });
-    AgoraRtcEngine.muteLocalAudioStream(muted);
+    _engine.muteLocalAudioStream(muted);
   }
 
   void _onSwitchCamera() {
-    AgoraRtcEngine.switchCamera();
+    _engine.switchCamera();
   }
 
- 
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -278,5 +269,4 @@ class _CallPageState extends State<CallPage> {
       ),
     );
   }
-
 }
